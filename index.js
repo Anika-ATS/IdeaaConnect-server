a88dc20b359417562dd237aaf095a223fc13b915
Anika-ATS <shuchi99anika@gmail.com>
2026-02-17 17:38:52 +0600

    setting

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-// require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+//load environment varriables from .env file
+dotenv.config()
+
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to IdeaaConnect Server!');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
