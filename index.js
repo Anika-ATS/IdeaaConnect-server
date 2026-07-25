a4a0c61dce758a488d75557374d8d6b5c912d427
Anika-ATS <shuchi99anika@gmail.com>
2026-07-25 16:00:26 +0600

    edited

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+const app = express();
+const port =process.env.PORT || 3000
+
 //load environment varriables from .env file
 dotenv.config()
 
 
-const app = express();
+// const app = express();
 
 // Middleware
 app.use(cors());
@@ -17,9 +20,12 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Welcome to IdeaaConnect Server!');
 });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
